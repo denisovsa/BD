51d06187f0492d20d3105789eda1d700ab305be3
GECorpse <lackoftallent17@gmail.com>
2023-06-02 14:08:56 +0300

    Final1

--- DB/Service.cs
+++ DB/Service.cs
@@ -26,7 +26,7 @@ namespace Kingsman.DB
         public decimal Cost { get; set; }
         public string Description { get; set; }
         public Nullable<int> ServiceTypeID { get; set; }
-        public byte[] Image { get; set; }
+        public string Photo { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<MaterialService> MaterialService { get; set; }
